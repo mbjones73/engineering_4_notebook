bdfa9313a4de93838014695d7c58a24bfd60d718
Scoot <56690169+mbjones73@users.noreply.github.com>
2022-01-27 10:04:08 -0500

    Add files via upload

---
+++ camera.py
@@ -0,0 +1,12 @@
+import time
+import picamera
+
+print('running')
+with picamera.PiCamera() as camera:
+    camera.resolution = (1024, 768)
+    camera.start_preview()
+    # Camera warm-up time
+    time.sleep(2)
+    camera.capture('foo.jpg')
+print('done')
+
